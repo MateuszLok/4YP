3b204c80c2fb84a248455b1b0cf9f5bf74cde255
MateuszLok <mateusz.pek@gmail.com>
2015-10-14 22:51:23 +0100

    Multiple dimensions added for calculations (not fully functional)

--- GP_functions.py
+++ GP_functions.py
@@ -6,20 +6,60 @@ from math import log10
 import matplotlib.pyplot as plt
 import scipy.optimize as optimize
 
-#Write
-def calculate_k(x1,x2,f,n,l):
+#Define input data
+def get_x_values():
+    return np.array([-1.5, -1.00, -0.75, -0.40, -0.25, 0.00])
+
+def get_y_values():
+    return np.array([-1.7, -1.25, -0.35, 0.1, 0.5, 0.75])
+
+"""def get_x_values():
+    return np.array(([-1.5],[-1.00],[-0.75],[-0.40],[-0.25],[0.0]),dtype=float)
+
+def get_y_values():
+    return np.array(([-1.7], [-1.25], [-0.35], [0.1], [0.5], [0.75]),dtype=float)"""
+
+
+
+#Calculate covariance for two values
+def calculate_k(x1,x2,f,n,ll):
     sigma_f = f
     sigma_n = n
+    l=ll
     if x1 == x2:
         return (sigma_f ** 2) * exp(-(x1-x2) ** 2 * (2 * l ** 2) ** -1) + sigma_n ** 2
     else:
         return (sigma_f ** 2) * exp(-(x1-x2) ** 2 * (2 * l ** 2) ** -1 )
 
-#Evaluates matrix K
+def calculate_kk(x1,x2,f,n,ll):
+    sigma_f = f
+    sigma_n = n
+    l=ll
+    difference = x1-x2
+    intermediate = -np.dot(difference.transpose(),difference) * (2 * l ** 2) ** -1
+    if x1.all() == x2.all():
+        return (sigma_f ** 2) * exp(intermediate) + sigma_n ** 2
+    else:
+        return (sigma_f ** 2) * exp(-difference.transpose() * difference * (2 * l ** 2) ** -1 ** -1)
+
+
+#Evaluates matrix K - covariance matrix
+def find_KK(vector_X, f, n, l):
+    sigma_f = f
+    l = l
+    sigma_n = n
+    print len(vector_X)
+    outcome = np.zeros((len(vector_X),len(vector_X)))
+    for i in range(0, len(vector_X)):
+        for j in range(0,len(vector_X)):
+            outcome[i][j] = (calculate_k(vector_X[i,:],vector_X[j,:],sigma_f,sigma_n,l))
+    return outcome
+
 def find_K(vector_X, f, n, l):
     sigma_f = f
     l = l
     sigma_n = n
+    print len(vector_X)
     outcome = np.zeros((len(vector_X),len(vector_X)))
     for i in range(0, len(vector_X)):
         for j in range(0,len(vector_X)):
@@ -36,6 +76,16 @@ def find_K_star(vector,x,f,n,l):
         outcome[0][i] = calculate_k(x,vector[i],sigma_f, sigma_n,l)
     return outcome
 
+def find_K_starr(vector,x,f,n,l):
+    sigma_f = f
+    l = l
+    sigma_n = n
+    outcome = np.zeros((1, len(vector)))
+    for i in range(0,len(vector)):
+        outcome[0][i] = calculate_k(x,vector[i,:],sigma_f, sigma_n,l)
+    return outcome
+
+
 #Evaluates matrix K**
 def find_K_2stars(x,f,n,l):
     sigma_f = f
@@ -45,11 +95,13 @@ def find_K_2stars(x,f,n,l):
 
 
 #----------------Optimization
+
+
 def function_to_minimize(input_data):
     f = input_data[0]
     n = input_data[1]
     l = input_data[2]
-    vector_x= [ -1.5, -1.00, -0.75, -0.40, -0.25, 0.00]
-    sample_y_opt = np.array([-1.7, -1.2, -0.35, 0.1, 0.5, 0.75])
+    vector_x= get_x_values()
+    sample_y_opt = np.array(get_y_values())
     sample_y_trans_opt = sample_y_opt.transpose()
     return 0.5 * np.dot(sample_y_trans_opt, np.dot(np.linalg.inv(find_K(vector_x,f,n,l)), sample_y_opt)) +0.5 * log10(np.linalg.det(find_K(vector_x,f,n,l))) + log10(6.28)
--- function.py
+++ function.py
@@ -10,15 +10,37 @@ import GP_functions as gp
 
 # ----------------------------------------------------------------------------
 
+
 #Example of x and y vectors
-sample_vector = [ -1.5, -1.00, -0.75, -0.40, -0.25, 0.00]
-sample_y = [-1.7, -1.2, -0.35, 0.1, 0.5, 0.75]
+sample_vector = gp.get_x_values()
+sample_y = gp.get_y_values()
+new_x = np.array([0.2])
+
+
+
+
+#Optimization part
+
+#result = optimize.minimize(gp.function_to_minimize, [3,0.5,3])
+
+#wynik = result['x']
+#print wynik
+"""sigma_f = wynik[0]     1.935
+sigma_n = wynik[1]
+length = wynik[2]"""
+
+sigma_f = 1.27
+sigma_n = 0.3
+length = 1
+
+
 
 #Finding values of K matrices for new values of x
-K = gp.find_K(sample_vector,1.27,0.3,1.0)
-K_star = gp.find_K_star(sample_vector,0.2,1.27,0.3,1.0)
-K_2stars = gp.find_K_2stars(0.2,1.27,0.3,1.0)
+K = gp.find_K(sample_vector,sigma_f,sigma_n,length)
+K_star = gp.find_K_star(sample_vector,new_x,sigma_f,sigma_n,length)
+K_2stars = gp.find_K_2stars(new_x,sigma_f,sigma_n,length)
 
+print K
 #Find y*
 K_inv = np.linalg.inv(K)
 X = np.dot(K_star,K_inv)
@@ -31,11 +53,7 @@ y_star_var = K_2stars-np.dot(K_star,np.dot(K_inv,K_star_trans))
 
 #--------------------------------
 
-#Optimization part
 
-result = optimize.minimize(gp.function_to_minimize, [3,0.5,3])
-
-wynik = result['x']
 
 
 
@@ -46,8 +64,8 @@ new_values = np.arange(-2,0.5,0.001)
 estimated_values_y = []
 estimated_variance_y = []
 for number in new_values:
-    K_star_estimate = gp.find_K_star(sample_vector,number,1.27,0.3,1.0)
-    K_2stars_estimate = gp.find_K_2stars(number,1.27,0.3,1.27)
+    K_star_estimate = gp.find_K_star(sample_vector,number,sigma_f,sigma_n,length)
+    K_2stars_estimate = gp.find_K_2stars(number,sigma_f,sigma_n,length)
     X_estimate = np.dot(K_star_estimate,K_inv)
     # Without conversion to float we have a list of arrays
     estimated_values_y.append(float(np.dot(X_estimate,sample_y)))
@@ -59,12 +77,11 @@ for number in new_values:
 #Plotting estimated curve
 plt.plot(new_values,estimated_values_y)
 plt.errorbar(new_values,estimated_values_y, yerr=estimated_variance_y, capsize=0)
-
 #Plotting one new value
 plt.plot(sample_vector, sample_y, 'ro')
 #plt.axis([min(sample_vector)-0.5, max(sample_vector)+0.5, min(sample_y)-0.5, max(sample_y)+0.5])
 plt.axis([-1.7,0.4,-2.6,1.7])
-plt.errorbar(sample_vector, sample_y, yerr=0.3, fmt='ro', capsize=0)
-plt.plot([0.2],[y_star], 'go')
+#plt.errorbar(sample_vector, sample_y, yerr=0.3, fmt='ro', capsize=0)
+#plt.plot([0.2],[y_star], 'go')
 plt.show()
 
